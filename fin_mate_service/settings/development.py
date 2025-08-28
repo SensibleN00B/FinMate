a0fb9b5a82998980ba3c6c6d6e7fe50f44e693f7
SensibleN00B <sensible.noob@gmail.com>
2025-08-28 20:03:19 +0300

    chore: updated manage.py, settings and requirements.txt

--- fin_mate_service/settings/development.py
+++ fin_mate_service/settings/development.py
@@ -13,8 +13,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 ACCOUNT_DEFAULT_HTTP_PROTOCOL = "http"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST", default="localhost"),
+        "PORT": env("POSTGRES_DB_PORT", default="5432"),
     }
 }
